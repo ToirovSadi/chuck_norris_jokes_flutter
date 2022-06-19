95e63cec2dca8ebfc48ab6c40f0c20985ef1fd84
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-19 19:48:03 +0300

    feat: add new colors for app

---
+++ lib/style/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const lightGreen = Color(0xFFC4FFA9);
+
+const lightGreenSearch = Color.fromARGB(255, 220, 255, 203);
+
+// background color
+const customGreen = Color(0xFF65FF52);
+
+// color for text style
+const customBlack = Color.fromARGB(255, 0, 0, 0);
+
+// color for text style in categories
+const customIndigo = Color.fromARGB(255, 63, 81, 181);
