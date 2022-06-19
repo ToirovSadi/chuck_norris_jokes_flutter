ab3cbf357be8509791eee9b81fe7cc9d3f0493aa
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-19 19:44:58 +0300

    fix: add categoryList to choose

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,1 +1,20 @@
 const apiUrl = 'https://api.chucknorris.io';
+
+const categoryList = [
+  "animal",
+  "career",
+  "celebrity",
+  "dev",
+  "explicit",
+  "fashion",
+  "food",
+  "history",
+  "money",
+  "movie",
+  "music",
+  "political",
+  "religion",
+  "science",
+  "sport",
+  "travel"
+];
